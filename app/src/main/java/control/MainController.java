68f194387033e1f05ecee04422dcfa926fc1be8e
Norman Seidel <norman.seidel@gmail.com>
2015-02-05 21:57:10 +0100

    move controller to control package

--- app/src/main/java/control/MainController.java
+++ app/src/main/java/control/MainController.java
@@ -1,10 +1,8 @@
-/*
+package control;/*
  * Created by norman on 01.02.15.
  */
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 
 /**
